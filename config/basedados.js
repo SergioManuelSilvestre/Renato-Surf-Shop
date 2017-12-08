3de3090676b4c6838ad6897c545921428d5552cb
Sergio Silvestre <silvestre_sergio1@hotmail.com>
2017-12-08 19:30:02 +0000

    server-test

---
+++ config/basedados.js
@@ -0,0 +1,21 @@
+const crypto = require('crypto') // .randomBytes(255).toString('hex');
+
+/*
+        // versão com teste // cyfer node doc
+crtypto comverte e cria
+
+
+*/
+crypto.randomBytes(256, (err, buf) => {
+    if (err) throw err;
+    console.log(`${buf.length} bytes of random data: ${buf.toString('hex')}`);
+  });
+
+// exporta o modulo no index temos que importar
+
+module.exports = {
+   uri: 'mongodb://localhost/test',
+   secret: 'crypto' ,
+   db:'Renato-shop'
+
+}
---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const config = require('./config/basedados'); // link para pasta config base de dados
+const mongoose = require('mongoose');
+const path = require ('path');
+mongoose.Promise = global.Promise;
+
+mongoose.connect(config.uri, (err) => {
+    if (err) {
+            console.log('Não foi possivel estabelecer ligação',err);       
+    } else {
+        console.log ('conexão estabelecida');
+        //console.log (config.secret);
+    }
+});
+
+
+ // mongoose.connect('mongodb://localhost/test', { useMongoClient: true });
+
+app.use(express.static(__dirname + '/surfShop/dist/')); // usar o ficheiro do angular em modo produção
+
+/**
+ // mensagem e inicio
+app.get('/',(req, res) => {  
+    res.send('hello world \n <h1>Servidor </h1> '); // para enviar um ficheiro
+  });
+  
+  console.log('a escutar e vivo no port 8080'); 
+  app.listen(8080);
+
+ */
+
+ app.get('*', (req, res) => {
+     res.sendFile(path.join(__dirname + '/surfShop/dist/index.html'));
+     
+ })
+
+ // para enviar um ficheiro
+
+
+console.log('a escutar e vivo no port 8080'); 
+app.listen(8080);
